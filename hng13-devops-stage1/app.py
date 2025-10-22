58c69dfef3dfdffd17ce81978b7e40f3398ae80a
dejinassar <dejinassar@gmail.com>
2025-10-22 08:36:49 +0100

    Update to DevOps Stage 1 with new index.html

--- hng13-devops-stage1/app.py
+++ hng13-devops-stage1/app.py
@@ -1,21 +1,10 @@
 from flask import Flask, render_template
-from datetime import datetime
 
 app = Flask(__name__)
 
-# ---------- Deployment Info ----------
-DEVOPS_STAGE = "Stage 1"
-DEV_NAME = "Ibrahim Nassar"
-DEPLOY_DATE = datetime.now().strftime("%d/%m/%Y")
-
 @app.route('/')
 def home():
-    return render_template(
-        "index.html",
-        stage=DEVOPS_STAGE,
-        dev_name=DEV_NAME,
-        deploy_date=DEPLOY_DATE
-    )
+    return render_template("index.html", stage="1", name="Ibrahim Nassar")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
