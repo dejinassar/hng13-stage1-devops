82f37d5bf10e6a472163e41d6649c4b993882b83
dejinassar <dejinassar@gmail.com>
2025-10-22 08:09:37 +0100

    Update Stage to Stage 1 and add dynamic template

--- hng13-devops-stage1/app.py
+++ hng13-devops-stage1/app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+
 app = Flask(__name__)
+
+# ---------- Deployment Info ----------
+DEVOPS_STAGE = "Stage 1"
+DEV_NAME = "Ibrahim Nassar"
+DEPLOY_DATE = datetime.now().strftime("%d/%m/%Y")
 
 @app.route('/')
 def home():
-    return "Hello from HNG DevOps Stage 1"
+    return render_template(
+        "index.html",
+        stage=DEVOPS_STAGE,
+        dev_name=DEV_NAME,
+        deploy_date=DEPLOY_DATE
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
